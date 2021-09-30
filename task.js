1fdbc5d68a13cb84d7910e85305ef1223df1d0f6
Mustafa Bin Hadi <91639104+mustafabinhadi@users.noreply.github.com>
2021-10-01 00:49:53 +0300

    Add files via upload

--- task.js
+++ task.js
@@ -11,11 +11,14 @@ let numbers = [
 
 console.log("Original numbers list: ", numbers)
 
+// soritng array elements in ACD order
+// the issue was in if condition using i instead of j
+
 for(let i = 0 ; i < numbers.length ; i++)
 {
     for(let j = 0 ; j < numbers.length - 1 ; j++)
     {
-        if(numbers[i] > numbers[j + 1])
+        if(numbers[j] > numbers[j + 1])
         {
             let temp = numbers[j];
             numbers[j] = numbers[j + 1]
@@ -26,11 +29,12 @@ for(let i = 0 ; i < numbers.length ; i++)
 
 console.log("Numbers list After sorting: ", numbers)
 
+// soritng array elements in DESC order 
 for(let i = 0 ; i < numbers.length ; i++)
 {
     for(let j = 0 ; j < numbers.length - 1 ; j++)
     {
-        if(numbers[i] < numbers[j + 1])
+        if(numbers[j] < numbers[j + 1])
         {
             let temp = numbers[j];
             numbers[j] = numbers[j + 1]
@@ -42,3 +46,58 @@ for(let i = 0 ; i < numbers.length ; i++)
 console.log("Numbers list After Desc sorting: ", numbers)
 
 
+// Task solution
+// Reading the txt file 
+var fs = require ('fs')
+var txt_file = fs.readFileSync('input.txt')
+
+// Sort the data that token from a file in an array
+let array=[]
+// Convert the data type of array (string to numbers) 
+array=txt_file.split(',')
+array= array.map(elem=> parseInt(elem, 10))
+
+
+
+// Function to sort array elements in ACD order
+function asc(numbers){
+    for(let i = 0 ; i < numbers.length ; i++)
+	{
+		for(let j = 0 ; j < numbers.length - 1 ; j++)
+		{
+			if(numbers[j] > numbers[j + 1])
+			{
+				let temp = numbers[j];
+				numbers[j] = numbers[j + 1]
+				numbers[j + 1] = temp;
+			}
+		}
+	}
+console.log("Numbers list After Asc sorting: ", numbers)
+}
+
+// Function to sort array elements in DESC order
+function desc(numbers){
+    for(let i = 0 ; i < numbers.length ; i++)
+	{
+		for(let j = 0 ; j < numbers.length - 1 ; j++)
+		{
+			if(numbers[j] < numbers[j + 1])
+			{
+				let temp = numbers[j];
+				numbers[j] = numbers[j + 1]
+				numbers[j + 1] = temp;
+			}
+		}
+	}
+    
+    console.log("Numbers list After Desc sorting: ", numbers)
+}
+
+
+console.log("immutable numbers list: ", numbers)
+
+// to sort the array in ACD order, then save it in a new file 'output.txt'
+
+asc(array)
+fs.writeFileSync('output.txt', array.toString());
